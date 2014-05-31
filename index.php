96e61eb14f2e73885acb5bfab08e282a79335cde
Alexander <alexander.db27@gmail.com>
2014-05-31 14:07:59 -0400

    updated index.php

--- index.php
+++ index.php
@@ -9,27 +9,6 @@
         require_once 'mysqltrick.php';
         
         $connection = new mysqlTrickClient();
-        
-        $db = $connection->selectDB("social");
-        
-        $users = $db->selectTable("users");
-        
-        $variables = array('email'=>'alexander.db27@gmail.com', 'id'=>'AlexPaTruco');
-        $cur = $users->select("WHERE email = :email", $variables);
-        
-        while ($cur->hasNext()) {
-        print_r($cur->getNext());
-        }
-        //echo $users->insert(array("firstname"=>"hello", "fecha"=>"nose"));
-        
-        
-        /*$conn = new MongoClient();
-        
-        $db = $conn->selectDB("names");
-        
-        $collection = $db->selectCollection("name");
-        
-        $collection->*/
         ?>
     </body>
 </html>
